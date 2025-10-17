4f80c26a34a93aa38ae674862f687b49a2095faa
q122858 <q122858@DESKTOP-7EIQ3KN>
2025-10-17 22:05:32 +0800

    加入專案檔案。

---
+++ File content modification/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace File_content_modification
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.txtFind = new System.Windows.Forms.TextBox();
+            this.txtReplace = new System.Windows.Forms.TextBox();
+            this.btnReplace = new System.Windows.Forms.Button();
+            this.lblFind = new System.Windows.Forms.Label();
+            this.lblReplace = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblFolder = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.Location = new System.Drawing.Point(13, 13);
+            this.btnSelectFolder.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(164, 48);
+            this.btnSelectFolder.TabIndex = 0;
+            this.btnSelectFolder.Text = "選擇資料夾";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // txtFind
+            // 
+            this.txtFind.Location = new System.Drawing.Point(108, 90);
+            this.txtFind.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtFind.Name = "txtFind";
+            this.txtFind.Size = new System.Drawing.Size(148, 27);
+            this.txtFind.TabIndex = 1;
+            // 
+            // txtReplace
+            // 
+            this.txtReplace.Location = new System.Drawing.Point(108, 137);
+            this.txtReplace.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtReplace.Name = "txtReplace";
+            this.txtReplace.Size = new System.Drawing.Size(148, 27);
+            this.txtReplace.TabIndex = 2;
+            // 
+            // btnReplace
+            // 
+            this.btnReplace.Location = new System.Drawing.Point(13, 189);
+            this.btnReplace.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnReplace.Name = "btnReplace";
+            this.btnReplace.Size = new System.Drawing.Size(164, 48);
+            this.btnReplace.TabIndex = 3;
+            this.btnReplace.Text = "執行批次替換";
+            this.btnReplace.UseVisualStyleBackColor = true;
+            this.btnReplace.Click += new System.EventHandler(this.btnReplace_Click);
+            // 
+            // lblFind
+            // 
+            this.lblFind.AutoSize = true;
+            this.lblFind.Location = new System.Drawing.Point(13, 93);
+            this.lblFind.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblFind.Name = "lblFind";
+            this.lblFind.Size = new System.Drawing.Size(87, 16);
+            this.lblFind.TabIndex = 4;
+            this.lblFind.Text = "查找文字：";
+            // 
+            // lblReplace
+            // 
+            this.lblReplace.AutoSize = true;
+            this.lblReplace.Location = new System.Drawing.Point(13, 145);
+            this.lblReplace.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblReplace.Name = "lblReplace";
+            this.lblReplace.Size = new System.Drawing.Size(87, 16);
+            this.lblReplace.TabIndex = 5;
+            this.lblReplace.Text = "替換文字：";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(305, 67);
+            this.lblStatus.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(75, 16);
+            this.lblStatus.TabIndex = 6;
+            this.lblStatus.Text = "處理狀態:";
+            // 
+            // lblFolder
+            // 
+            this.lblFolder.AutoSize = true;
+            this.lblFolder.Location = new System.Drawing.Point(196, 29);
+            this.lblFolder.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblFolder.Name = "lblFolder";
+            this.lblFolder.Size = new System.Drawing.Size(103, 16);
+            this.lblFolder.TabIndex = 7;
+            this.lblFolder.Text = "資料夾選擇：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 257);
+            this.Controls.Add(this.lblFolder);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblReplace);
+            this.Controls.Add(this.lblFind);
+            this.Controls.Add(this.btnReplace);
+            this.Controls.Add(this.txtReplace);
+            this.Controls.Add(this.txtFind);
+            this.Controls.Add(this.btnSelectFolder);
+            this.Font = new System.Drawing.Font("新細明體", 12F);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "查找文字後替換";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.TextBox txtFind;
+        private System.Windows.Forms.TextBox txtReplace;
+        private System.Windows.Forms.Button btnReplace;
+        private System.Windows.Forms.Label lblFind;
+        private System.Windows.Forms.Label lblReplace;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblFolder;
+    }
+}
+
---
+++ File content modification/Form1.cs
@@ -0,0 +1,86 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace File_content_modification
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region 選擇資料夾
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.CheckFileExists = false;
+                openFileDialog.ValidateNames = false;
+                openFileDialog.FileName = "選擇資料夾";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string folderPath = System.IO.Path.GetDirectoryName(openFileDialog.FileName);
+                    lblFolder.Text += folderPath;
+                }
+            }
+
+        }
+
+        #endregion
+
+        #region 批次替換
+        private void btnReplace_Click(object sender, EventArgs e)
+        {
+            string folderPath = lblFolder.Text.Replace("資料夾選擇：", "").Trim();
+            string find = txtFind.Text;
+            string replace = txtReplace.Text;
+
+            ReplaceInAllWordDocuments(folderPath, find, replace);
+        }
+
+        // 批次處理資料夾
+        private void ReplaceInAllWordDocuments(string folderPath, string oldText, string newText)
+        {
+            string[] files = Directory.GetFiles(folderPath, "*.docx"); // Open XML 僅支援 .docx
+            foreach (var file in files)
+            {
+                try
+                {
+                    ReplaceTextInWord(file, oldText, newText);
+                    lblStatus.Text += $"\r\n {Path.GetFileName(file)} ✅";
+                }
+                catch (Exception ex)
+                {
+                    lblStatus.Text += $"\r\n {Path.GetFileName(file)} ❌ {ex.Message}";
+                }
+            }
+        }
+
+
+        // 替換 Word 文件文字
+        private void ReplaceTextInWord(string filePath, string oldText, string newText)
+        {
+            using (WordprocessingDocument wordDoc = WordprocessingDocument.Open(filePath, true))
+            {
+                var body = wordDoc.MainDocumentPart.Document.Body;
+
+                foreach (var text in body.Descendants<DocumentFormat.OpenXml.Wordprocessing.Text>())
+                {
+                    if (text.Text.Contains(oldText))
+                    {
+                        text.Text = text.Text.Replace(oldText, newText);
+                    }
+                }
+                wordDoc.MainDocumentPart.Document.Save();
+            }
+        }
+        #endregion
+
+    }
+}
